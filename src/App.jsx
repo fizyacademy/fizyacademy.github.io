d71b7a0b9c352fd14948b2831966009c6c5f30aa
hussein2007 <hussein.bakr2007@gmail.com>
2025-07-30 17:46:57 +0300

    Added StudentStatisticsPage component and route to App.jsx, updated database file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import Loading from "./components/Loading";
 import { AuthProvider, useAuth } from "./AuthContext";
 import AccountSettings from "./Pages/AccountSettings";
+import StudentStatisticsPage from './Pages/StudentStatisticsPage';
 
 const Student = lazy(() => import("./Pages/Student"));
 const Admin = lazy(() => import("./Pages/Admin"));
@@ -34,6 +35,7 @@ function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/account" element={<AccountSettings />} />
+        <Route path="/my-progress" element={<StudentStatisticsPage />} />
         <Route path="/*" element={RoleComponent} />
       </Routes>
 
